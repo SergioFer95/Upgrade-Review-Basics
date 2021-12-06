bcdbd30e4bfb9ce10a4dbf1efb5a3a4ab5683607
Sergio Fernández <sergio.fernndez@bootcamp-upgrade.com>
2021-12-06 17:33:17 +0100

    Nothing

--- review_basics.js
+++ review_basics.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // Iteración #1: Mix for e includes
-// Dado el siguiente javascript usa forof para recorrer el array de películas, genera un nuevo array con las categorías de las películas e imprime por consola el array de categorías. Ten en cuenta que las categorías no deberían repetirse. Para filtrar las categorías puedes ayudarte de la función .includes()
+// Dado el siguiente javascript usa for of para recorrer el array de películas, genera un nuevo array con las categorías de las películas e imprime por consola el array de categorías. Ten en cuenta que las categorías no deberían repetirse. Para filtrar las categorías puedes ayudarte de la función .includes()
 const movies = [
     {title: 'Madaraspar', duration: 192, categories: ['comedia', 'aventura']},
     {title: 'Spiderpan', duration: 122, categories: ['aventura', 'acción']},
